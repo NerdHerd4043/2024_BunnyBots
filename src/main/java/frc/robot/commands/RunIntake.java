2281c1c0bd3b75a225f22c8f0d51960445581f9f
Senor-Pie <espenpbock@gmail.com>
2024-11-21 20:52:32 -0800

    11/21 Espen - RunIntake Command

--- src/main/java/frc/robot/commands/RunIntake.java
+++ src/main/java/frc/robot/commands/RunIntake.java
@@ -1,5 +1,39 @@
 package frc.robot.commands;
 
-public class RunIntake {
-    
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.Intake;
+
+public class RunIntake extends Command {
+    public final Intake intake;
+    public final double subIntakeMotorSpeed;
+    public final double mainIntakeMotorSpeed;
+    public final double indexMotorSpeed;
+
+    public RunIntake(Intake intake, double subIntakeMotorSpeed, double mainIntakeMotorSpeed, double indexMotorSpeed) {
+        this.intake = intake;
+        this.subIntakeMotorSpeed = subIntakeMotorSpeed;
+        this.mainIntakeMotorSpeed = mainIntakeMotorSpeed;
+        this.indexMotorSpeed = indexMotorSpeed;
+
+        addRequirements(this.intake);
+    }
+
+    @Override
+    public void initialize() {
+    }
+
+    @Override
+    public void execute() {
+        intake.runIntake(subIntakeMotorSpeed, mainIntakeMotorSpeed, indexMotorSpeed);
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+        intake.stopIntake();
+    }
+
+    @Override
+    public boolean isFinished() {
+        return false;
+    }
 }
